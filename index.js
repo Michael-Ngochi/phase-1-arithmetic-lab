c0e21e32a043cbfc4ac981457c3139f39b088796
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-10 12:41:45 +0300

    added arithmetic operators

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1=Number.parseInt(2,10);
+let num2=Number.parseInt(31,10);
+let num3=10;
+let num4=6;
+let multiply=num1*num2;
+
+//random value
+let random=Math.floor(Math.random()*9)+1;
+
+//modulus operation
+let mod=num3%num4;
+
+//max function
+let max= Math.max(1,5,10,15,20)
